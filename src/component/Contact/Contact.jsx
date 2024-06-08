2d9c8b3d5a986af7960c31151d9ef0fa3b842389
utsavsarkar <utsavsarkar2000@gmail.com>
2024-06-08 17:57:56 +0530

    added alert

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -5,55 +5,64 @@ import Button from "@mui/material/Button";
 
 function Contact() {
   return (
-    <center style={{ marginTop: 50 }}>
-      <b style={{ color: "#50c878" }}>Have Any Questions?</b>
-      <h1>GET IN TOUCH</h1>
-      <p>
-        <LocationOnIcon />
-        201/9, Bakshi House 40-41, Nehru Place, New Delhi - 110019
-      </p>
+    <div style={{ marginTop: 100 }}>
+      <center>
+        <b style={{ color: "#50c878" }}>Have Any Questions?</b>
+        <h1>GET IN TOUCH</h1>
+        <p>
+          <LocationOnIcon />
+          201/9, Bakshi House 40-41, Nehru Place, New Delhi - 110019
+        </p>
 
-      <div style={{ width: "60%" }}>
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="inputGroup-sizing-default">Name</InputGroup.Text>
-          <Form.Control
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </InputGroup>
-        <br />
+        <div style={{ width: "80%" }}>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Name
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <br />
 
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="inputGroup-sizing-default">
-            Email
-          </InputGroup.Text>
-          <Form.Control
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </InputGroup>
-        <br />
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Email
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <br />
 
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="inputGroup-sizing-default">
-            Mobile
-          </InputGroup.Text>
-          <Form.Control
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </InputGroup>
-        <br />
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Mobile
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <br />
 
-        <InputGroup>
-          <InputGroup.Text>Message</InputGroup.Text>
-          <Form.Control as="textarea" aria-label="With textarea" />
-        </InputGroup>
-        <br />
+          <InputGroup>
+            <InputGroup.Text>Message</InputGroup.Text>
+            <Form.Control as="textarea" aria-label="With textarea" />
+          </InputGroup>
+          <br />
 
-        <Button variant="contained">Submit</Button>
-      </div>
-    </center>
+          <Button
+            variant="contained"
+            onClick={() => alert("Currently Site is on build!")}
+          >
+            Submit
+          </Button>
+        </div>
+      </center>
+    </div>
   );
 }
 
