27db7905daea1e902d81566b2a76e68abc04f4e2
utsavsarkar <utsavsarkar2000@gmail.com>
2024-06-08 17:07:21 +0530

    added material ui

--- src/component/CustomCards/CustomCards.jsx
+++ src/component/CustomCards/CustomCards.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import NotListedLocationIcon from "@mui/icons-material/NotListedLocation";
+import SchoolIcon from "@mui/icons-material/School";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import "./CustomCards.css";
+import { Stack } from "react-bootstrap";
 
 function CustomCards() {
   const items = [
@@ -11,12 +18,32 @@ function CustomCards() {
     "CUSTOMER",
   ];
 
+  function renderIcon(name) {
+    switch (name) {
+      case "SOLUTIONS":
+        return <EmojiObjectsIcon style={{ fontSize: "3em" }} />;
+      case "SERVICES":
+        return <CleaningServicesIcon style={{ fontSize: "3em" }} />;
+      case "SUCCESS":
+        return <EmojiEventsIcon style={{ fontSize: "3em" }} />;
+      case "SUPPORT":
+        return <NotListedLocationIcon style={{ fontSize: "3em" }} />;
+      case "KNOWLEDGE":
+        return <SchoolIcon style={{ fontSize: "3em" }} />;
+      case "CUSTOMER":
+        return <SupportAgentIcon style={{ fontSize: "3em" }} />;
+    }
+  }
+
   return (
     <div className="container">
       {items.map((item) => {
         return (
           <div key={item} className="box">
-            {item}
+            <Stack>
+              <div>{renderIcon(item)}</div>
+              {item}
+            </Stack>
           </div>
         );
       })}
